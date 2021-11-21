a196da0c69ccfea4eafa8ba7b13dc07503fd117e
MmrMahfuj <mahfuj.mmr3@gmail.com>
2021-11-21 19:56:40 +0600

    react firebase authentication

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA22dmSp5UCGJfs07rl6XYipYY48sWwC-k",
+    authDomain: "react-firebase-authentic-98aed.firebaseapp.com",
+    projectId: "react-firebase-authentic-98aed",
+    storageBucket: "react-firebase-authentic-98aed.appspot.com",
+    messagingSenderId: "1081340692106",
+    appId: "1:1081340692106:web:31f1447d1af175828c8f73"
+};
+
+export default firebaseConfig;
